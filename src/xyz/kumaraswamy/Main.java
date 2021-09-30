ba9c2ff5eb8068f81d4b3ab0c514abb6ad7ad6dc
Pratyush Dev <66131007+PraDevel@users.noreply.github.com>
2021-09-30 19:49:29 +0530

    Update Main.java
    Fixed a path issue

--- src/xyz/kumaraswamy/Main.java
+++ src/xyz/kumaraswamy/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         final File script = new File(
-                "C:\\Users\\user\\Documents\\projects\\WifiTest\\shell\\script.sh"
+                "../../../shell/script.sh"
         );
         final Desktop desktop = Desktop.getDesktop();
         desktop.open(script);
